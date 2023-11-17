2c346a98379f4cc2be2422af3c3ebfb76ee69209
haolin <c21325063@mytudublin.ie>
2023-11-17 14:05:09 +0000

    port

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ CORS(app)
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'secret'
 app.config['MYSQL_DB'] = 'student'
-app.config['MYSQL_HOST'] = '40.90.237.122:3306'
+app.config['MYSQL_HOST'] = '40.90.237.122'
 mysql.init_app(app)
 
 @app.route("/add") #Add Student
